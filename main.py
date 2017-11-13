8d993e240ba3ad196b0c8ee16f145aa22d77198e
jerryxu178 <lx46@cornell.edu>
2017-11-12 20:57:53 -0500

    added main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import nltk
 from nltk import sent_tokenize, word_tokenize, pos_tag
-text = "Machine learning is Michael Scott the science of getting computers to act without being explicitly programmed. In the past decade, machine learning has given us self-driving cars, practical speech recognition, effective web search, and a vastly improved understanding of the human genome. Machine learning is so pervasive today that you probably use it dozens of times a day without knowing it. Many researchers also think it is the best way to make progress towards human-level AI. In this class, you will learn about the most effective machine learning techniques, and gain practice implementing them and getting them to work for yourself. More importantly, you'll learn about not only the theoretical underpinnings of learning, but also gain the practical know-how needed to quickly and powerfully apply these techniques to new problems. Finally, you'll learn about some of Silicon Valley's best practices in innovation as it pertains to machine learning and AI."
+text = "he was a good customer at Mcdonalds and GoodWill"
 sents = sent_tokenize(text) # build list of sentences
 tokens = word_tokenize(text) # build list of words
 tagged_tokens = pos_tag(tokens) # tag tokens
-
 #nltk.help.upenn_tagset('JJ')
 
 
@@ -19,4 +18,22 @@ st = StanfordNERTagger('stanford-ner/classifiers/english.all.3class.distsim.crf.
 					   'stanford-ner/stanford-ner.jar',
 					   encoding='utf-8')
 classified = st.tag(tokens)
+
+
 print classified
+
+
+
+
+
+def main():
+	print "ask me a question"
+	while True:
+		statement = raw_input(">")
+		print generate_response(statement.lower())
+		if statement.lower() == "quit":
+			break
+
+if __name__ == "__main__":
+	main()
+
