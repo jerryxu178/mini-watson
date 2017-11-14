e034d93004198079ae4f0b7af826e75499d8e242
jerryxu178 <lx46@cornell.edu>
2017-11-14 13:50:12 -0500

    implementing question processing

---
+++ parse_question.py
@@ -0,0 +1,62 @@
+import nltk
+import constants
+from constants import common_verbs, keyword_tags
+
+def process_question(question):
+	tokens = nltk.word_tokenize(question)
+	tagged_tokens = nltk.pos_tag(tokens)
+
+	# use helper functions to parse question here
+	answer_type = get_answer_type(question.lower())
+	keywords = get_keywords(tagged_tokens)
+	proper_nouns = get_proper_n(tagged_tokens)
+	# use all other nouns if no proper nouns found
+	if len(proper_nouns) == 0:
+		sep_keywords = reprioritize_nouns(keywords)
+		proper_nouns = sep_keywords[0]
+		keywords = sep_keywords[1]
+		"""
+		(proper_nouns, keywords) = reprioritize_nouns(keywords)
+		"""
+	return (answer_type, keywords, proper_nouns)
+
+def get_answer_type(question):
+	if "who" in question:
+		return "PERSON"
+	elif "when" in question:
+		return "TIME"
+	elif "where" in question:
+		return "PLACE"
+	else:
+		return "ANY"
+
+def get_keywords(tagged_tokens):
+	keywords = []
+	for token in tagged_tokens:
+		if token[0] not in common_verbs and token[1] in keyword_tags:
+			keywords.append(token)
+	return keywords
+
+def get_proper_n(tagged_tokens):
+	named_entities = []
+	curr_token = ""
+	for token in tagged_tokens:	
+		if token[1] == "NNP" or token[1] == "NNPS":
+			curr_token += " " + token[0]
+		else:
+			if curr_token != "":
+				named_entities.append(curr_token.strip())
+				curr_token = ""
+	if curr_token != "":
+		named_entities.append(curr_token.strip())
+	return named_entities
+
+def reprioritize_nouns(tagged_tokens):
+	noun_list = []
+	for token in tagged_tokens:
+		if token[1] == "NN" or token[1] == "NNS":
+			noun_list.append(token[0])
+			tagged_tokens.remove(token)
+	return (noun_list, tagged_tokens)
+
+process_question("who is John Ben eating")
