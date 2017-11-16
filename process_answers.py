0d7129924e4ffd7c0346ae6b1d42f1f8467dddcc
jerryxu178 <lx46@cornell.edu>
2017-11-16 15:53:35 -0500

    refined answer retrieval

--- process_answers.py
+++ process_answers.py
@@ -1,7 +1,8 @@
-
+#
 def prepare_answers(scored_answers, question):
 	filtered_answers = remove_unwanted_ans(scored_answers, question)
 	filtered_answers = combine_duplicate_ans(filtered_answers)
+	filtered_answers = combine_similar_answers(filtered_answers)
 	filtered_answers.sort(key=lambda ans: ans[1]) 
 	answers = []
 	for elt in filtered_answers:
@@ -14,16 +15,39 @@ def remove_unwanted_ans(scored_answers, question):
 	for answer in scored_answers:
 		if answer[1] != 0 and answer[0] not in question:
 			filtered_answers.append(answer)
+	if len(filtered_answers) == 0:
+		return scored_answers
 	return filtered_answers
 
 def combine_duplicate_ans(scored_answers):
 	answers_dict = {}
 	for answer in scored_answers:
 		if answer[0] in answers_dict:
-			answers_dict[answer[0]] += answer[1]
+			if answer[1] == 0:
+				answers_dict[answer[0]] += 1
+			else:
+				answers_dict[answer[0]] += answer[1]
 		else:
-			answers_dict[answer[0]] = answer[1]
+			if answer[1] == 0:
+				answers_dict[answer[0]] = 1
+			else:
+				answers_dict[answer[0]] = answer[1]
 	ans_list = []
 	for ans in answers_dict:
 		ans_list.append((ans, answers_dict[ans]))
 	return ans_list
+
+def combine_similar_answers(scored_answers):
+	for ans in scored_answers:
+		for other_ans in scored_answers:
+			if ans[0] != other_ans[0] and ans[0] in other_ans[0]:
+				scored_answers.append((other_ans[0],other_ans[1] + ans[1]))
+				try:
+					scored_answers.remove(ans)
+				except ValueError: 
+					pass
+				try:
+					scored_answers.remove(other_ans)
+				except ValueError: 
+					pass
+	return scored_answers
