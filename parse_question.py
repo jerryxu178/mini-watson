c34785ebf0376a97fe5f6927bb002ef38a3ad68c
jerryxu178 <lx46@cornell.edu>
2017-11-21 15:54:04 -0500

    small update

--- parse_question.py
+++ parse_question.py
@@ -3,6 +3,9 @@ import constants
 from constants import common_verbs, keyword_tags
 
 def process_question(question):
+	"""
+	TODO
+	"""
 	tokens = nltk.word_tokenize(question)
 	tagged_tokens = nltk.pos_tag(tokens)
 	# use helper functions to parse question here
@@ -11,16 +14,15 @@ def process_question(question):
 	proper_nouns = get_proper_n(tagged_tokens)
 	# use all other nouns if no proper nouns found
 	if len(proper_nouns) == 0:
-
 		sep_keywords = reprioritize_nouns(keywords)
 		proper_nouns = sep_keywords[0]
 		keywords = sep_keywords[1]
-		
-		#(proper_nouns, keywords) = reprioritize_nouns(keywords)
-		
 	return (answer_type, keywords, proper_nouns)
 
 def get_answer_type(question):
+	"""
+	TODO
+	"""
 	if "who" in question:
 		return "PERSON"
 	elif "when" in question:
@@ -31,6 +33,9 @@ def get_answer_type(question):
 		return "ANY"
 
 def get_keywords(tagged_tokens):
+	"""
+	TODO
+	"""
 	keywords = []
 	for token in tagged_tokens:
 		if token[0] not in common_verbs and token[1] in keyword_tags:
@@ -38,6 +43,9 @@ def get_keywords(tagged_tokens):
 	return keywords
 
 def get_proper_n(tagged_tokens):
+	"""
+	TODO
+	"""
 	named_entities = []
 	curr_token = ""
 	for token in tagged_tokens:	
@@ -52,6 +60,9 @@ def get_proper_n(tagged_tokens):
 	return named_entities
 
 def reprioritize_nouns(tagged_tokens):
+	"""
+	TODO
+	"""
 	noun_list = []
 	revised_tagged_tokens = []
 	for token in tagged_tokens:
@@ -60,5 +71,3 @@ def reprioritize_nouns(tagged_tokens):
 		else:
 			revised_tagged_tokens.append(token)
 	return (noun_list, revised_tagged_tokens)
-
-process_question("who, the man on the Moon is eating")
