42600a2b3a2f70eff0e5c4075078f2da2be569c7
jerryxu178 <lx46@cornell.edu>
2017-12-01 09:08:21 -0500

    updated documentation

--- constants.py
+++ constants.py
@@ -1,11 +1,11 @@
-# all constants for trivia-bot are written here
+# all constants for trivia-bot here
 
-# 
+# common question words that determine what the question is asking for
 question_words = ["who", "what", "when", "where"]
 
-#
+# words with the listed POS tags are to be considered as keywords
 keyword_tags = ["CD", "JJ", "JJR", "JJS", "NN", "NNS", "PDT", "RB", "RBR", 
 "RBS", "RP", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
 
-#
+# common verbs are not to be used as keywords
 common_verbs = ["is", "are", "was", "were", "am", "are"]
