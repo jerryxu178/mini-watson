8a18253ea71ba2c191cb0e64b18d5cb91b7d9251
jerryxu178 <lx46@cornell.edu>
2017-12-26 23:19:15 -0500

    fixed punctuation

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from wiki_search import search_wikipedia
 from process_answers import prepare_answers
 
 def main():
-	print "Hello! Please ask me a question"
+	print "Hello! Please ask me a question."
 	while True:
 		question = raw_input(">")		
 		(answer_type, keywords, proper_nouns) = process_question(question)
 		if len(proper_nouns) == 0:
 			# no nouns found in the question
-			print "I don't understand the question's subject, please try again"
+			print "I don't understand the question's subject, could you rephrase it?"
 			continue
 		scored_answers = search_wikipedia(answer_type, keywords, proper_nouns)
 		answer_list = prepare_answers(scored_answers, question)
 		if len(answer_list) == 0:
 			# searching Wikipedia yielded no results
-			print "Sorry, I did not find any potential answers"
+			print "Sorry, I did not find any potential answers."
 			continue
 		print "Is the answer " + answer_list[-1] + "? [y/n]"
 		reply = raw_input(">")
@@ -28,7 +28,7 @@ def main():
 				print "Hmm... I am stumped!"
 		else:
 			print "Unexpected response, I was expecting 'y' or 'n'"
-		print "Please feel free to ask me another question"
+		print "Please feel free to ask me another question."
 
 if __name__ == "__main__":
 	main()
